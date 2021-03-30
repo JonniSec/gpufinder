639811f3d8c33103607618ea16713d452c1d70b0
Jonni <cohenj14@gmail.com>
2021-03-30 15:47:26 -0700

    Update main.py
    Leo update

--- main.py
+++ main.py
@@ -1,26 +1,25 @@
 import requests
 from bs4 import BeautifulSoup
-import json
-from pandas import DataFrame as df
 
-page = requests.get("https://www.newegg.com/p/pl?N=100007709%20601357282%20601321572%20601359511%20601341679")
+page = requests.get(
+    "https://www.newegg.com/p/pl?N=100007709%20601341679%20600007308%20600007306%20601296707%20601331379%20601357282%20601359511%20600286767%20600083901%20600007320%20600007315&PageSize=96")
 soup = BeautifulSoup(page.text, 'html.parser')
-
 
 page.encoding = 'ISO-885901'
 soup = BeautifulSoup(page.text, 'html.parser')
 
-item_tree_list = soup.find_all(class_ = 'btn btn-primary btn-mini')
-for i in item_tree_list[:4]:
-  print(i)
+divTag = soup.find_all("div", {"class": "item-container"})
 
+candidates = set()
+want = {'550', '3070'}
+for tag in divTag:
+    tdTags = tag.select("button", {"class": "btn-primary"})
+    tag_contents = tdTags[0].contents
+    if 'View Details' in str(tag_contents[0]):
+        continue
+    tag_link = tdTags[0].parent.parent.parent.parent.contents[0]
+    for wanted in want:
+        if wanted in tag_link.attrs['href']:
+            candidates.add(tag_link)
 
-links = soup.find_all(class_ = 'item-title')
-for i in links[:4]:
-  print(i)
-
-if not item_tree_list:
-    print("nothing")
-
-if item_tree_list:
-    print("something in stock")
+print(candidates)
