7d5fb2120be90d472bb7fdbebfc4896533c2a2c7
Andrew <ajcachiaras@gmail.com>
2021-03-30 19:21:58 -0400

    Jonni is bad

--- main.py
+++ main.py
@@ -1,26 +1,23 @@
 import requests
 from bs4 import BeautifulSoup
-import json
-from pandas import DataFrame as df
+from contextlib import redirect_stdout
 
-page = requests.get("https://www.newegg.com/p/pl?N=100007709%20601357282%20601321572%20601359511%20601341679")
+
+page = requests.get("https://www.newegg.com/p/pl?N=100007709%20601341679%208000%20600007308%20600007306%20601296707%20601331379%20601357282%20601359511&PageSize=96")
 soup = BeautifulSoup(page.text, 'html.parser')
-
 
 page.encoding = 'ISO-885901'
 soup = BeautifulSoup(page.text, 'html.parser')
 
-item_tree_list = soup.find_all(class_ = 'btn btn-primary btn-mini')
-for i in item_tree_list[:4]:
-  print(i)
+divTag = soup.find_all("div", {"class": "item-container"})
+
+for tag in divTag:
+    tdTags = tag.select("button", {"class": "btn-primary"})
+    print(tdTags[0].contents, tdTags[0].parent.parent.parent.parent.contents[0])
 
 
-links = soup.find_all(class_ = 'item-title')
-for i in links[:4]:
-  print(i)
 
-if not item_tree_list:
-    print("nothing")
 
-if item_tree_list:
-    print("something in stock")
+with open('./myfile.txt', 'w') as f:
+    with redirect_stdout(f):
+        print(tdTags[0].contents, tdTags[0].parent.parent.parent.parent.contents[0])
